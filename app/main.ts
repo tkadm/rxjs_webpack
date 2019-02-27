916922d62919742db0b5c46d332058a28f733ca6
Константин Михайлович Тихомиров <15783@tyazhmash.com>
2019-02-27 11:17:47 +0400

    Переработка оснастики тестирования

--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,44 @@
 import * as Rx from 'rxjs';
+import { map, take } from 'rxjs/operators';
 import { Export } from './operators/common';
 
-let entry_point: (obs: Rx.Observable<Event>, bnt: HTMLButtonElement) => void = Export;
+export interface IButtons {
+    Main: HTMLButtonElement;
+    Complete: HTMLButtonElement;
+    Error: HTMLButtonElement;
+}
+
+let entry_point: (obs: Rx.Observable<Event>, buttons: IButtons) => void = Export;
 
 window.addEventListener('load', () => {
-    let btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("btnRoot");
-    let btn2: HTMLButtonElement = <HTMLButtonElement>document.getElementById("btnAdd");
-    if (entry_point) entry_point(Rx.fromEvent(btn, 'click'), btn2);
+    let btnMain: HTMLButtonElement = <HTMLButtonElement>document.getElementById("btnMain");
+    let btnComplete: HTMLButtonElement = <HTMLButtonElement>document.getElementById("btnComplete");
+    let btnError: HTMLButtonElement = <HTMLButtonElement>document.getElementById("btnError");
+    if (entry_point) entry_point(Rx.fromEvent(btnMain, 'click'), { Main: btnMain, Complete: btnComplete, Error: btnError });
 });
 
+export function CreateMarketInterval(interval: number, count: number, prefix?: string): Rx.Observable<string> {
+    return Rx.interval(interval).pipe(map(value => prefix ? prefix + value : value.toString()), take(count));
+}
 
-//import { Example } from './operators/combineAll';
-//import { Example } from './operators/combineLatest';
-// import { Example } from './operators/take';
-// Example();
+export function CreateMarketEvent(observ_event: Rx.Observable<Event>, prefix?: string): Rx.Observable<string> {
+    let i: number = 0;
+    return observ_event.pipe(map(_ => { i++; return prefix ? prefix + i : i.toString(); }));
+}
+
+export function SubscribeConsole<T>(obs: Rx.Observable<T>): Rx.Subscription {
+    return obs.subscribe(value => console.log(value), error => console.warn(error), () => console.log('Complete!'));
+}
+
+export function CreateButtonObserver(next: HTMLButtonElement, complete: HTMLButtonElement, error?: HTMLButtonElement): Rx.Observable<number> {
+    return new Rx.Observable(subscriber => {
+        let i: number = 0;
+        next.addEventListener('click', _ => { i++; subscriber.next(i); });
+        complete.addEventListener('click', () => subscriber.complete());
+        if (error) error.addEventListener('click', error => subscriber.error(error));
+    });
+}
+
+export function CreateIButtonObserver(buttons: IButtons): Rx.Observable<number> {
+    return CreateButtonObserver(buttons.Main, buttons.Complete, buttons.Error);
+}
--- app/operators/common.ts
+++ app/operators/common.ts
@@ -1,15 +1,10 @@
 import * as Rx from 'rxjs';
-import { take, map, every, concatAll } from 'rxjs/operators';
+import { debounce } from 'rxjs/operators';
+
+import { IButtons, CreateMarketEvent, CreateMarketInterval, SubscribeConsole } from '../main';
 
 
-export function Export(obs: Rx.Observable<Event>, btn: HTMLButtonElement) {
-    let array: Array<number> = [0, 0];
-    let obsA: Rx.Observable<string> = obs.pipe(map(_ => { array[0]++; return "A" + array[0] }));
-    //let obsB: Rx.Observable<string> = Rx.fromEvent(btn, 'click').pipe(map(_ => { array[1]++; return "B" + array[1] }));
-    let second = Rx.interval(1000).pipe(take(5), map(item => "Значение: " + item));
-    let outer = Rx.interval(1000).pipe(take(2), map(_ => _ == 0 ? second : obsA));
-
-    //Rx.concat(Rx.interval(1000).pipe(take(5), map(item => "Значение: " + item)), obsA)
-    //Rx.combineLatest(obsA, obsB, (valA, valB) => [valA, valB, 100])
-    outer.pipe(concatAll()).subscribe(value => console.log(value), null, () => console.log("complete!"));
+export function Export(obs: Rx.Observable<Event>, buttons:IButtons) {
+    
 }
+
