24b6cea556cc27c4fc3fb8bb7b636a5dfa0bd311
Константин Михайлович Тихомиров <15783@tyazhmash.com>
2019-03-04 16:40:17 +0400

    window

--- app/common.ts
+++ app/common.ts
@@ -1,10 +1,10 @@
 import * as Rx from 'rxjs';
-import { map, take, sample, skipLast, tap, takeLast } from 'rxjs/operators';
+import { map, take, timestamp, toArray } from 'rxjs/operators';
 
 import { IButtons, CreateMarkedEvent, CreateMarkedInterval, SubscribeConsole, CreateIButtonObserver, GenerateRandom, Letters } from './main';
 
 export function Export(obs: Rx.Observable<Event>, buttons: IButtons) {
-    let result = CreateIButtonObserver(buttons).pipe(takeLast(4));
+    let result = Rx.range(1,9).pipe(toArray());
 
     SubscribeConsole(result);
 }
