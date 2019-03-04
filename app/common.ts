7fee6635a2c87460ec194d715b4b5bfdf40c71c0
Константин Михайлович Тихомиров <15783@tyazhmash.com>
2019-03-04 14:20:51 +0400

    throttle

--- app/common.ts
+++ app/common.ts
@@ -1,10 +1,10 @@
 import * as Rx from 'rxjs';
-import { map, take, sample } from 'rxjs/operators';
+import { map, take, sample, skipLast, tap, takeLast } from 'rxjs/operators';
 
 import { IButtons, CreateMarkedEvent, CreateMarkedInterval, SubscribeConsole, CreateIButtonObserver, GenerateRandom, Letters } from './main';
 
 export function Export(obs: Rx.Observable<Event>, buttons: IButtons) {
-    let result = Rx.interval(1000).pipe(sample(CreateIButtonObserver(buttons)));
+    let result = CreateIButtonObserver(buttons).pipe(takeLast(4));
 
     SubscribeConsole(result);
 }
