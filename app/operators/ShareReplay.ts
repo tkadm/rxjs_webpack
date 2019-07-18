bb4be500d539a11caf52ccf50e57a3aea0253b3e
Константин Михайлович Тихомиров <15783@tyazhmash.com>
2019-07-18 12:35:03 +0400

    no

--- app/operators/ShareReplay.ts
+++ app/operators/ShareReplay.ts
@@ -1,9 +1,10 @@
 import * as Rx from 'rxjs';
 
-import { map, take, timestamp, withLatestFrom, zipAll, pluck, filter, first, switchMap, debounceTime, tap, startWith, publish, share, multicast, refCount } from 'rxjs/operators';
+import { map, take, timestamp, withLatestFrom, zipAll, pluck, filter, first, switchMap, debounceTime, tap, startWith, publish, share, multicast, refCount,
+shareReplay } from 'rxjs/operators';
 
 import { IButtons, CreateMarkedEvent, CreateMarkedInterval, SubscribeConsole, CreateIButtonObserver, GenerateRandom, Letters, details } from '../main';
 
 export function ShareReplayExport(obs: Rx.Observable<Event>, buttons: IButtons, buttonsII: IButtons) {
-
+    obs.pipe(shareReplay());
 }
