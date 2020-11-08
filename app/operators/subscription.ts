e726719753064d41e5edabd46194af89b57d7025
Тихомиров Константин <tdka97@yandex.ru>
2020-11-08 16:13:30 +0400

    В процессе: subscription.ts

--- app/operators/subscription.ts
+++ app/operators/subscription.ts
@@ -2,6 +2,16 @@ import * as Rx from 'rxjs';
 import { subscribeOn } from 'rxjs/operators';
 import { IButtons } from '../main';
 
+export class ListenedObservable<T> extends Rx.Observable<T>{
+    // subscribe(observer?: Rx.PartialObserver<T>): Rx.Subscription<T>{
+
+    // }
+    // subscribe(next?: (value: T) => void, error?: (error: any) => void, complete?: () => void): Subscription{
+
+    // }
+}
+
+
 export function Export(obs: Rx.Observable<Event>, buttons: IButtons, buttons2: IButtons) {
     obs.subscribe(console.log);
     let sbs: Rx.Observable<any> = Rx.fromEvent(buttons.Next, 'click');
