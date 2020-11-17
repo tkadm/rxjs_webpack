42257f03979ff809366cb44a3ea9a439c37b477d
Константин Михайлович Тихомиров <15783@tyazhmash.com>
2020-11-17 11:10:10 +0400

    Добавил комент. с rexexp даты строки в ISO

--- app/webSocket.ts
+++ app/webSocket.ts
@@ -1,8 +1,15 @@
 import * as rx from 'rxjs';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { map } from 'rxjs/operators';
+
+/**
+ * Регулярное вражение для строки даты в ISO формате
+ * ^\d{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1,2]\d|3[0,1])T(?:0[1-9]|1\d|2[0-3]):[0-5]\d:[0-5]\d(?:.\d+|)\+(?:[0,1]\d|2[0,3]):[0-5]\d
+ */
 
 export function onWSClick() {
     console.log("WebSocket!!!");
     let socket: WebSocketSubject<any> = webSocket("ws://localhost:60541/");
-    socket.subscribe(value => { console.log(value); }, error => { console.error(error); }, () => { console.log("socket competed!"); });
+    socket.pipe(map(item => { item.JSMoment = new Date(item.Moment); return item; }))
+        .subscribe(value => { console.log(value); }, error => { console.error(error); }, () => { console.log("socket competed!"); });
 }
